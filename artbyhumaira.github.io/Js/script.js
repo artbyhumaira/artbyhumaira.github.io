feb7deeb7758dcd142ad031f01e4f07a091fbeb0
Naeem Uddin <87529483+artbyhumaira@users.noreply.github.com>
2022-01-03 18:11:31 +0500

    Delete artbyhumaira.github.io directory

--- artbyhumaira.github.io/Js/script.js
+++
@@ -1,8 +0,0 @@
-// for mobile version which is not ready yet, write URL between "" to redirect
-if (screen.width <= 699) {
-	document.location = "";
-};
-
-if ((screen.width => 700 {
-	document.location = "www.artbyhumaira.ga";
-})) {}

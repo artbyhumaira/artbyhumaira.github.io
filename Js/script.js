f9da60b368d855a54d74baabadb80cecc1ee3c87
Naeem Uddin <87529483+artbyhumaira@users.noreply.github.com>
2022-01-03 18:12:15 +0500

    Add files via upload

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+// for mobile version which is not ready yet, write URL between "" to redirect
+if (screen.width <= 699) {
+	document.location = "";
+};
+
+if ((screen.width => 700 {
+	document.location = "www.artbyhumaira.ga";
+})) {}
